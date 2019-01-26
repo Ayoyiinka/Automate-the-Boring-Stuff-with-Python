a2c39989019352c5db765e087ce65f10abf9e1d4
Ayoyiinka <31844291+Ayoyiinka@users.noreply.github.com>
2019-01-26 20:44:56 +0100

    Update chapter7RegularExpressions.py

--- chapter7RegularExpressions.py
+++ chapter7RegularExpressions.py
@@ -1,9 +1,7 @@
-##Strong Password Detection
-Write a function that uses regular expressions to make sure the password
-string it is passed is strong. A strong password is defined as one that is at
-least eight characters long, contains both uppercase and lowercase characters,
-and has at least one digit. You may need to test the string against multiple
-regex patterns to validate its strength.'''
+# Strong Password Detection
+'''Write a function that uses regular expressions to make sure the password string it is passed is strong. A strong password is defined 
+as one that is at least eight characters long, contains both uppercase and lowercase characters, and has at least one digit. You may 
+need to test the string against multiple regex patterns to validate its strength.'''
 
 import re
 
@@ -25,17 +23,12 @@ def strong_password(password):
             
     else:
         return 'Weak Password'
-
-
 print(strong_password(password))
 
-
-##Regex Version of strip()
-Write a function that takes a string and does the same thing as the strip()
-string method. If no other arguments are passed other than the string to
-strip, then whitespace characters will be removed from the beginning and
-end of the string. Otherwise, the characters specified in the second argument
-to the function will be removed from the string.
+# Regex Version of strip()
+'''Write a function that takes a string and does the same thing as the strip() string method. If no other arguments are passed other 
+than the string to strip, then whitespace characters will be removed from the beginning and end of the string. Otherwise, the characters
+specified in the second argument to the function will be removed from the string.'''
 
 def stripper(first, second = ''):
     if second == '':
